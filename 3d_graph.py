25abaa9f73d983bca7ceb06bbdbb7c99dafd5cf7
koisoysauce <kai.jp2014@gmail.com>
2026-05-19 14:34:35 -0500

    Add images and change name for cleaner execution/readability

---
+++ 3d_graph.py
@@ -0,0 +1,22 @@
+import numpy as np
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+
+# plt.style.use('dark_background')
+fig = plt.figure(figsize = (7, 6))
+ax = plt.axes(projection='3d')
+
+# ax.xaxis.pane.set_facecolor((0, 0, 0, 0))
+# ax.yaxis.pane.set_facecolor((0, 0, 0, 0))
+# ax.zaxis.pane.set_facecolor((0, 0, 0, 0))
+
+# Initiate function f(x, y) = sin(x)cos(y)
+x = np.arange(-5, 5, 0.1)
+y = np.arange(-5, 5, 0.1)
+X, Y = np.meshgrid(x, y) # Returns two 2D arrays for defining our function
+Z = np.sin(X) * np.cos(Y)
+# Plot function
+ax.plot_surface(X, Y, Z, cmap = plt.cm.cividis) # adding _surface covers the space in between lines
+# ax.set_aspect('equal')
+
+plt.show()
