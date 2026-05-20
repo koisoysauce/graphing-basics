3e3cc683654166590fd5faba54e704ed215b5b0a
Shinko83 <suttonjayden957@gmail.com>
2026-05-20 12:12:07 +1000

    add arcsin and linear graph

--- graph_2.py
+++ graph_2.py
@@ -5,6 +5,20 @@ x = np.arange(-2, 2, 0.01)
 y = x ** 2
 fig, ax = plt.subplots()
 ax.plot(x, y)
+plt.title("x**2")
+plt.show()
+
+np.arcsin
+x = np.arange(-1, 1, 0.01)
+y = np.arcsin(x)
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set_ybound(-1, 1)
+plt.title("arcsin x")
+plt.show()
+
+fig, ax = plt.subplots()
+ax.plot(x, x)
 plt.show()
 
 # Parameters for standard form: (x^2/a^2) - (y^2/b^2) = 1
@@ -22,4 +36,9 @@ ax.contour(X, Y, F, [1], colors='blue')
 ax.set_xbound(-10, 10)
 ax.set_ybound(-10, 10)
 plt.title("Hyperbola")
+plt.show()
+
+x = np.arange(1, 1, 0.01)
+fig, ax = plt.subplots()
+ax.plot(x, x)
 plt.show()
