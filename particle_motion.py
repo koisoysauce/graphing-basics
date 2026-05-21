4c45dfbd72a9dd4af7b890415b0fde5cfc2649cf
koisoysauce <kai.jp2014@gmail.com>
2026-05-21 12:38:38 -0500

    Add particle free fall simulation

--- particle_motion.py
+++ particle_motion.py
@@ -44,3 +44,65 @@ def update(frame): # How does the plot update? For loop through each element in 
 
 ani = FuncAnimation(fig, update, frames=np.linspace(-np.pi, np.pi, 100), init_func=init, blit=False, interval=30)
 plt.show()
+
+# Animation of particle free falling in a vacuum closed system
+# acceleration of gravity is 9.8 m/s^2
+def euler_step(z, step):
+    global vel
+    vel += step * -9.8
+    z.append(z[-1] + step * vel)
+
+def init():
+    x.clear()
+    x.append(0)
+    y.clear()
+    y.append(0)
+    z.clear()
+    z.append(100)
+    particle.set_data(x, y)
+    particle.set_3d_properties(z)
+    trail.set_data(x, y)
+    trail.set_3d_properties(z)
+    return particle, trail
+
+def update(frame):
+    if z[-1] <= 0: # resets simulation when the particle goes below z=0
+        init()
+        global vel
+        vel = 0
+        return particle, trail
+    
+    x.append(0)
+    y.append(0)
+    euler_step(z, step)
+    particle.set_data([x[-1]], [y[-1]])
+    particle.set_3d_properties([z[-1]])
+    trail.set_data(x, y)
+    trail.set_3d_properties(z)
+    return particle, trail
+
+x = [0]
+y = [0]
+z = [100]
+vel = 0
+step = 0.01
+fig = plt.figure(figsize=(7,6))
+ax = plt.axes(projection='3d')
+
+ax.set_xlim(-20, 20)
+ax.set_ylim(-20, 20)
+ax.set_zlim(0, 100)
+ax.set_aspect('equal')
+ax.xaxis.pane.set_facecolor((0, 0, 0, 0))
+ax.yaxis.pane.set_facecolor((0, 0, 0, 0))
+ax.zaxis.pane.set_facecolor((0, 0, 0, 0))
+ax.xaxis._axinfo["grid"].update(linewidth=0.1)
+ax.yaxis._axinfo["grid"].update(linewidth=0.1)
+ax.zaxis._axinfo["grid"].update(linewidth=0.1)
+
+particle, = ax.plot(x[-1], y[-1], z[-1], 'bo')
+trail, = ax.plot(x, y, z, 'b-', alpha=0.5)
+
+ani = FuncAnimation(fig, func=update, frames=1000, init_func=init, blit=False, interval=50)
+plt.tight_layout()
+plt.show()
